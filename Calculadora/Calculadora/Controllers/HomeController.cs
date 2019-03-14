47d4ff39d997dcff72876d779bf264b7f973d01a
Luis Miguel Barbado BadaloLus <aluno19991@ipt.pt>
2019-03-14 17:09:32 +0000

    Versão Inicial do controller

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -11,6 +11,10 @@ namespace Calculadora.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //Iniciar o visor com o '0'
+            ViewBag.Ecra = "0";
+            //Vars auxiliares
+            Session["primeiraVezOperador"] = true;
             return View();
         }
 
@@ -18,6 +22,58 @@ namespace Calculadora.Controllers
         [HttpPost]
         public ActionResult Index(string bt, string visor)
         {
+            //var auxiliar
+            string ecra = visor;
+
+            //Identificar o valor na variável 'bt'
+            switch (bt)
+            {
+                //Se entrei aqui, é porque foi selecionado um 'algarismo'
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+
+                    //Vou decidir o que fazer quando o visor só existe o 'zero'
+                    
+                    if (visor.Equals("0")) //visor =="0"
+                    {
+                        ecra = bt;
+                    }
+                    else
+                    {
+                        ecra = ecra + bt;
+                    }
+                            
+                    break;
+                case ",":
+                    //Processar o caso da ','
+                    if (!visor.Contains(",")) ecra = ecra + bt;
+                    break;
+                //Se entrei aqui, é porque foi selecionado um 'algarismo'
+                case "+":
+                case "-":
+                case "x":
+                case ":":
+                    //É a primeira vez que carreguei num destes operadores?
+                    if((bool)Session["primeiraVezOperador"] == true) { // == true é facultativo
+
+                    };
+                    break;
+            }
+            //prepara o conteúdo a aparecer no VISOR da View
+            ViewBag.Ecra = ecra;
+
+
+
+
+
             return View();
         }
     }
