1ef09adc3c722313f496ed291b74b054136b7a01
Luis Miguel Barbado BadaloLus <aluno19991@ipt.pt>
2019-03-21 16:21:00 +0000

    Conclução dos operadores + css nos botões

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace Calculadora.Controllers
             ViewBag.Ecra = "0";
             //Vars auxiliares
             Session["primeiraVezOperador"] = true;
+            Session["iniciarOperando"] = false;
+
             return View();
         }
 
@@ -29,7 +31,7 @@ namespace Calculadora.Controllers
             //Identificar o valor na variável 'bt'
             switch (bt)
             {
-            
+
                 //Se entrei aqui, é porque foi selecionado um 'algarismo'
                 case "0":
                 case "1":
@@ -43,8 +45,8 @@ namespace Calculadora.Controllers
                 case "9":
 
                     //Vou decidir o que fazer quando o visor só existe o 'zero'
-                    
-                    if (visor.Equals("0")) //visor =="0"
+
+                    if ((bool)Session["iniciarOperando"] || visor.Equals("0")) //visor =="0"
                     {
                         ecra = bt;
                     }
@@ -52,41 +54,88 @@ namespace Calculadora.Controllers
                     {
                         ecra = ecra + bt;
                     }
-                            
+                    Session["iniciarOperando"] = false;
+                    break;
+                //Alterar o valor de positivo para negativo
+                case "+/-":
+                    if (!visor.Contains("-"))
+                    {
+                        ecra = "-" + visor;
+                    }
+                    else
+                    {
+                        ecra = trocavalor;
+                    }
                     break;
                 case ",":
                     //Processar o caso da ','
-                    if (!visor.Contains(",")) ecra = ecra + bt;
+                    if (!visor.Contains(","))
+                    {
+                        ecra = ecra + bt;
+                    }
                     break;
                 //Se entrei aqui, é porque foi selecionado um 'operador'
                 case "+":
                 case "-":
                 case "x":
                 case ":":
-                
+                case "=":
                     //É a primeira vez que carreguei num destes operadores?
-                    if ((bool)Session["primeiraVezOperador"] == true) { // == true é facultativo
+                    if ((bool)Session["primeiraVezOperador"]) // == true é facultativo
+                    { 
+                        Session["primeiraVezOperador"] = false;
+                    }
+                    else
+                    {
+                        // Recuperar os valores dos operandos
+                        double operador1 = Convert.ToDouble((string)Session["primeiroOperando"]);
+                        double operador2 = Convert.ToDouble(visor);
+                        switch ((string)Session["operadorAnterior"])
+                        {
+                            case "+":
+                                visor = operador1 + operador2 + "";
+                                break;
+                            case "-":
+                                visor = operador1 - operador2 + "";
+                                break;
+                            case "x":
+                                visor = operador1 * operador2 + "";
+                                break;
+                            case ":":
+                                visor = operador1 / operador2 + "";
+                                break;
+                        }
 
-                    };
+                    }
+                    ecra = visor;
+                    Session["primeiroOperando"] = visor;
+                    // limpar display
+                    Session["iniciarOperando"] = true;
+
+                    if (bt.Equals("="))
+                    {
+                        //Marcar o operador como primeiro operando
+                        Session["primeiraVezOperador"] = true;
+                    }
+                    else
+                    {
+                        // Guardar o valor do operador
+                        Session["operadorAnterior"] = bt;
+                        Session["primeiraVezOperador"] = false;
+                    }
+                    //Guardar o display para utilização futura
+                    Session["primeiroOperando"] = ecra;
+                    Session["iniciarOperando"] = true;
                     break;
                 //Apagar o valor dentro do ecrã
                 case "C":
-                    ecra = "";
-                break;
-                //Alterar o valor de positivo para negativo
-                case "+/-":
-                    if (!visor.Contains("-"))
-                        ecra = "-" + visor;
-                    else
-                        ecra = trocavalor;
+                    ecra = "0";
+                    Session["primeiraVezOperador"] = true;
+                    Session["iniciarOperando"] = false;
                     break;
-
             }
             //prepara o conteúdo a aparecer no VISOR da View
             ViewBag.Ecra = ecra;
-
-
-
 
 
             return View();
